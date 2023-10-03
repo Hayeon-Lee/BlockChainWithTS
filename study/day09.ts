f3d64aaec4aae4cd768ceabea6984bfe06b9134e
Hayeon-Lee <papepopepe@naver.com>
2023-10-03 13:14:00 +0900

    feat: polymorphism

---
+++ study/day09.ts
@@ -0,0 +1,26 @@
+/*
+classes 그리고 interfaces 를 활용하여, 아래 API를 위한 '미니' 버전을 구현하세요.
+
+LocalStorage API
+Geolocation API
+
+LocalStorage API:
+추상화 클래스와 제네릭을 사용하세요.
+
+Usage:
+
+localStorage.setItem(<key>, <value>)
+localStorage.getItem(<key>)
+localStorage.clearItem(<key>)
+localStorage.clear()
+
+Geolocation API:
+overloading을 사용하세요.
+geolocation.getCurrentPosition(successFn);
+geolocation.getCurrentPosition(successFn, errorFn);
+geolocation.getCurrentPosition(successFn, errorFn, optionsObj);
+geolocation.watchPosition(success);
+geolocation.watchPosition(success, error);
+geolocation.watchPosition(success, error, options);
+geolocation.clearWatch(id);
+*/

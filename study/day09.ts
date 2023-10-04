a536dcaae10a862c2cf118fe86f78e1f12f5f664
Hayeon-Lee <papepopepe@naver.com>
2023-10-04 22:53:50 +0900

    feat: abstract, generic, overloading

--- study/day09.ts
+++ study/day09.ts
@@ -1,26 +1,71 @@
-/*
-classes 그리고 interfaces 를 활용하여, 아래 API를 위한 '미니' 버전을 구현하세요.
+interface SStorage<T> {
+  [key: string]: T;
+}
 
-LocalStorage API
-Geolocation API
+abstract class ALocalStorage<T> {
+  constructor(protected storage: SStorage<T> = {}) {}
 
-LocalStorage API:
-추상화 클래스와 제네릭을 사용하세요.
+  abstract setItem(key: string, value: T): void;
+  abstract getItem(key: string): T;
+  abstract clearItem(key: string): void;
+  abstract clear(): void;
+}
 
-Usage:
+class LocalStorage<T> extends ALocalStorage<T> {
+  setItem(key: string, value: T) {
+    this.storage[key] = value;
+    return;
+  }
+  getItem(key: string): T {
+    return this.storage[key];
+  }
+  clearItem(key: string) {
+    delete this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
 
-localStorage.setItem(<key>, <value>)
-localStorage.getItem(<key>)
-localStorage.clearItem(<key>)
-localStorage.clear()
+class geolocation<T> {
+  //optionObj는 보조 데이터이므로 존재 유무가 함수 플로우에 영향을 주지는 않으므로 검사하지 않는다
+  //사용된다면 successFn이나 errorFn에서 사용되거나 할텐데 이들의 구체적인 작동은 여기서 정의하지 않는다.
+  //getCurrentPosition은 성공 시 successFn()을, 실패 시 errorFn()을 실행한다
+  getCurrentPosition(
+    successFn: () => void,
+    errorFn?: () => void,
+    optionsObj?: [
+      maximumAge: number,
+      timeout: number,
+      enableHighAccuracy: boolean
+    ]
+  ): void {
+    if (errorFn !== undefined) {
+      try {
+        successFn();
+      } catch (e) {
+        errorFn();
+      }
+    } else successFn();
+  }
 
-Geolocation API:
-overloading을 사용하세요.
-geolocation.getCurrentPosition(successFn);
-geolocation.getCurrentPosition(successFn, errorFn);
-geolocation.getCurrentPosition(successFn, errorFn, optionsObj);
-geolocation.watchPosition(success);
-geolocation.watchPosition(success, error);
-geolocation.watchPosition(success, error, options);
-geolocation.clearWatch(id);
-*/
+  //성공 시 successFn()을, 실패 시 errorFn()을 실행한다
+  watchPosition(
+    successFn: () => number,
+    errorFn?: () => number,
+    optionsObj?: any[]
+  ): number {
+    if (errorFn !== undefined) {
+      try {
+        return successFn();
+      } catch (e) {
+        return errorFn();
+      }
+    } else return successFn();
+  }
+
+  //clearWatch()는 watchPosition()이 전달하는 정수를 ID로 받고 해당 ID의 위치 변화 감지를 해제합니다.
+  clearWatch(id: number): void {
+    return;
+  }
+}
